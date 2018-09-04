8788fa2698e212adff8c6175076f818fa28466fb
ImYourNightmare <42852787+ImYourNightmare@users.noreply.github.com>
2018-09-03 22:01:31 -0600

    Create Libreria.java

---
+++ Libreria.java
@@ -0,0 +1,51 @@
+package Proyecto1;
+
+public class Libreria extends Libro{
+	String Nombre, Pais, Ubicacion, Horario, Telefono;
+	Libro libro;
+	
+	public Libreria(Libro plibro,String pNombre,String pPais, String pUbicacion, String pHorario, String pTelefono) {
+		Nombre = pNombre;
+		Pais = pPais;
+		Ubicacion = pUbicacion;
+		Horario = pHorario;
+		Telefono = pTelefono;
+		libro = plibro;
+	}
+	public void setNombreLibreria(String pNombre) {
+		Nombre = pNombre;
+	}
+	public void setPais(String pPais) {
+		Pais = pPais;
+	}
+	public void setUbicacion(String pUbicacion) {
+		Ubicacion = pUbicacion;
+	}
+	public void setHorario(String pHorario) {
+		Horario = pHorario;
+	}
+	public void setTelefono(String pTelefono) {
+		Telefono = pTelefono;
+	}
+	public void setlibro(Libro plibro) {
+		libro = plibro;
+	}
+	public String getNombreLibreria() {
+		return Nombre;
+	}
+	public String getPais() {
+		return Pais;
+	}
+	public String getUbicacion() {
+		return Ubicacion;
+	}
+	public String getHorario() {
+		return Horario;
+	}
+	public String getTelefono() {
+		return Telefono;
+	}
+	public Libro getlibro() {
+		return libro;
+	}
+}
