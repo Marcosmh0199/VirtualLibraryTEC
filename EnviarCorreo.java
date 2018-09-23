7431e8e35c5deebb6258f540a62dd8fca2f5af18
ImYourNightmare <42852787+ImYourNightmare@users.noreply.github.com>
2018-09-23 10:42:29 -0600

    Add files via upload

---
+++ EnviarCorreo.java
@@ -0,0 +1,41 @@
+package control;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.Session;
+
+public class EnviarCorreo {
+	public final String usuario="proyectopython18@gmail.com";
+	public final String contraseña="python18pro";
+	String para="";
+	String asunto="";
+	String cuerpomensaje="";
+	
+	public EnviarCorreo(String para,String asunto,String cuerpomensaje) {
+		Properties propiedades = new Properties();
+		propiedades.put("mail.smtp.auth", "true");
+		propiedades.put("mail.smtp.starttls.enable", "true");
+		propiedades.put("mail.smtp.host", "smtp.gmail.com");
+		propiedades.put("mail.smtp.port", "587");
+		
+		Session session = Session.getInstance(propiedades, null);
+		
+		try {	 
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(usuario));
+            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(para));
+            message.setSubject(asunto);
+            message.setText(cuerpomensaje);
+            Transport.send(message,usuario,contraseña);
+            System.out.print("mensaje enviado");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+	}
+	public static void main(String[] args) {
+		EnviarCorreo corr = new EnviarCorreo("marcosmh0199@gmail.com","este es el asunto","este es el cuerpo");
+	}
+}
